ae62230c16ea7930cb04bdde023b82cf0feb111f
janaiturner <143103314+janaiturner@users.noreply.github.com>
2023-11-06 17:20:12 -0600

    added img

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 // JS scripts placed here
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
