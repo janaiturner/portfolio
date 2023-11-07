de7731de603f598646a1ec5870b8520b1e5d71ac
janaiturner <143103314+janaiturner@users.noreply.github.com>
2023-11-06 21:51:11 -0600

    fixing header

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 // JS scripts placed here
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
